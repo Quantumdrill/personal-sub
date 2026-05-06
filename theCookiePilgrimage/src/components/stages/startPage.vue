ad3570b376a29d330be10e0293c736ac2bba0c1f
Quantumdrill <quantumdrill317@gmail.com>
2026-05-06 14:19:21 -0400

    modify start page

--- theCookiePilgrimage/src/components/stages/startPage.vue
+++ theCookiePilgrimage/src/components/stages/startPage.vue
@@ -10,9 +10,8 @@ let router = useRouter()
         <ul>
             <br></br>
             <h4>Operating System Related</h4>
-            <li>If you are on <b>Windows</b>, you are good to go.</li>
+            <li>If you are on <b>Windows</b> or a Windows virtual machine, you are good to go.</li>
             <li>If you are on <b>macOS</b>, there are more limitations and due to an extremely strict browser and popup window policy on macOS:</li>
-            <li>The web experience is now currently only available on wide-screen desktop devices.</li>
             <ul>
                 <li>To interact with popup windows normally, open the dev tools of the browser, undock it and minimize it at all times during gameplay. <br>
                     To do this, Command + Option + I, then Command + Shift + D, then minimize the undocked window.</li>
@@ -20,6 +19,7 @@ let router = useRouter()
             </ul>
             <br></br>
             <h4>Browser Window Related</h4>
+            <li>The web experience is now currently only available on wide-screen desktop devices.</li>
             <li>The game is tested to be running flawlessly on Google Chrome and Microsoft Edge. Firefox might be laggy and unstable.</li>
             <li><b>Please keep the main browser window maximized at all times, and try not to resize or move it. </b></li>
             <li><b>But don't make the browser full screen by hitting F11 or the green button if on macOS, the url bar and other windows needs to be visible.</b></li>
